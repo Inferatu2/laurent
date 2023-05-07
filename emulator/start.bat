17fa9fb6b00eb9f20ee0714d5724490d98b1dc78
Inferatu2 <inferatu2@gmail.com>
2023-05-07 21:12:10 +0400

    правка эмуляторов

--- emulator/start.bat
+++ emulator/start.bat
@@ -2,4 +2,3 @@
 start python C:\Users\Inferatu\PycharmProjects\pythonProject8\emulator\laurent_emul.py
 start python C:\Users\Inferatu\PycharmProjects\pythonProject8\emulator\miner_emul1.py
 start python C:\Users\Inferatu\PycharmProjects\pythonProject8\emulator\miner_emul2.py
-@pause
