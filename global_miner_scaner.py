af311afeb60b2a458d8bafa8c3484085e2d3dd23
Inferatu2 <inferatu2@gmail.com>
2023-05-08 15:40:54 +0400

    заменил списки на словари

--- global_miner_scaner.py
+++ global_miner_scaner.py
@@ -2,28 +2,25 @@ import datetime
 import threading
 import time
 
-def all_miner_print(miners_status, sleep_time):
-        miners_status = [x for x in miners_status if x]
-        print('=============печатаем сведеную таблицу===============')
-        print(f"имя        аптайм     хешрейд              шары    битшары   карты")
-        for rig in miners_status:
-            if rig:
-                if not rig.online:
-                    print(f'{rig.name} offline  {datetime.datetime.now() - rig.time}')
-                else:
-                    print(
-                        f"{rig.name:<10} {rig.timeup:<10} {rig.obhash:<20} {rig.valid_share:<7} {rig.rez_share:<9}"
-                        f"{rig.card_count:<5}")
-        all_miner_analise(miners_status)
+def all_miner_check(miners_online, miners_offline, sleep_time):
+    print('\t =============печатаем сведеную таблицу===============')
+    print(f"\t имя        аптайм     хешрейд              шары    битшары   карты")
+    for rig_name, rig in miners_online.items():
+        print(
+            f"\t {rig.name:<10} {rig.timeup:<10} {rig.obhash:<20} {rig.valid_share:<7} {rig.rez_share:<9}"
+            f"{rig.card_count:<5}")
+    for rig_name, rig in miners_offline.items():
+        print(f'\t {rig.name} offline  {datetime.datetime.now() - rig.time}')
+    # all_miner_analise(miners_status)
 
 
-def all_miner_analise(miners_status):
-    middle_temp_list = []
-    miners_status_online = [x for x in miners_status if x.online]
-    for rig in miners_status_online:
-        if rig.card_count:
-            middle_temp_list.append(sum(map(int, rig.templist))/rig.card_count)  # middle temperage for this rig
-    print(f'онлайн {len(miners_status_online)}')
-    print(f'оффлайн {len(miners_status) - len(miners_status_online)}')
-    if middle_temp_list:
-        print(f'средняя температура карт {round(sum(middle_temp_list)/len(middle_temp_list))}')
+# def all_miner_analise(miners_status):
+#     middle_temp_list = []
+#     miners_status_online = [x for x in miners_status if x.online]
+#     for rig in miners_status_online:
+#         if rig.card_count:
+#             middle_temp_list.append(sum(map(int, rig.templist))/rig.card_count)  # middle temperage for this rig
+#     print(f'онлайн {len(miners_status_online)}')
+#     print(f'оффлайн {len(miners_status) - len(miners_status_online)}')
+#     if middle_temp_list:
+#         print(f'средняя температура карт {round(sum(middle_temp_list)/len(middle_temp_list))}')
--- main2.py
+++ main2.py
@@ -11,7 +11,7 @@ from config import conf_to_dict
 from miner import Miner
 
 
-def scan_miner(result, miner_count_number):
+def scan_miner(result):
     """Мы не закрываем подключение по сокету между опросами, оно постоянно открыто и майнер постоянно опрашивается
     поэтому есть переменная miner.err, если она пустая, значит майнер подключен, надо только првоести опрос,
     если она хоть что-то содержит, значит была ошибка и надо пытаться подключиться.
@@ -21,50 +21,50 @@ def scan_miner(result, miner_count_number):
         if not miner.online:
             miner.connect(int(configuration['miner_connect_time_out']))
         if not miner.online:
-            miner_offline(miner, miner_count_number)
+            miner_offline(miner)
             time.sleep(int(configuration['scan_time']))
             continue
             # тут надо запустиь процесс перезагрузки
         miner.get_start()
         if miner.err:
-            miner_offline(miner, miner_count_number)
+            miner_offline(miner)
             time.sleep(int(configuration['scan_time']))
             continue
             # ребутаем
         if not miner.err:
-            miner_online(miner, miner_count_number)
+            miner_online(miner)
         time.sleep(int(configuration['scan_time']))
 
 
 def global_miner():
     while True:
         with threading.Lock():
-            global_miner_scaner.all_miner_print(miners_status, int(configuration['all_miner_result']))
+            global_miner_scaner.all_miner_check(miners_online, miners_offline, int(configuration['all_miner_result']))
         time.sleep(int(configuration['all_miner_result']))
 
 
-def miner_offline(miner, miner_count_number):
-    miners_status[miner_count_number] = miner
+def miner_offline(miner):
+    miners_offline[miner.name] = miner
+    miners_online.pop(miner.name, '')
     print(f'майнер {miner.name} оффлайн {datetime.datetime.now() - miner.time}\n', end='')
 
 
-def miner_online(miner, miner_count_number):
+def miner_online(miner):
     print('Пoдключен ' + miner.name + '\n', end='')
     miner.api_to_class()  # распарсим список и сохраняем в атрибуты класса
     miner.print()  # выводим результат
-    miners_status[miner_count_number] = miner
+    miners_online[miner.name] = miner
+    miners_offline.pop(miner.name, '')
     miner.time = datetime.datetime.now()
 
 
 def main():
     global configuration
     global file_path_db
-    global miners_status
     global miners_online
     global miners_offline
     miners_online = {}
     miners_offline = {}
-    miners_status = []
     main_scan_work = False
     file_path = str(os.getcwd())
     file_path_conf = os.path.join(file_path, "config.txt")
@@ -77,12 +77,9 @@ def main():
         print('Подключились к базе данных')
         main_scan_work = True  # False если есть ошибка подключения к базе, не даст продолжить
         miner_list = data_base.get_miner_list()
-        miner_count_number = -1
         for miner in miner_list:
-            miner_count_number = miner_count_number + 1
-            miners_status.append('')
             result = data_base.get_one_miner(miner[0])  # miner[0] it's name of the miner
-            thr = threading.Thread(target=scan_miner, args=(result, miner_count_number), name=miner[0])
+            thr = threading.Thread(target=scan_miner, args=(result, ), name=miner[0])
             thr.start()
 
         threading.Timer(10, global_miner).start()
